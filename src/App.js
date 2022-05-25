774f9751cb53dd1ee2bc84f5b52c8dd6d024d8ea
Alisha <97678545+AlishaMohasin@users.noreply.github.com>
2022-05-25 13:16:03 +0530

    src

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react"
+import Todo from './components/Todo.jsx';
+
+function App() {
+  // fetch("https://m6g3bt.sse.codesandbox.io/todos")
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+  return (
+ <div className='App'>
+
+<Todo/>
+
+
+       </div>
+  );
+  }
+
+
+export default App;
